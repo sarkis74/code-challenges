16c09852181c7bb7f870d52929fc66cec9562341
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-02-25 17:52:14 -0800

    latest

--- 401-code-challenges/linked-list.js
+++ 401-code-challenges/linked-list.js
@@ -1,59 +1,65 @@
 'use strict';
 const Node = require('./node');
-console.log(Node);
 
 class LinkedList{
     constructor(){
         this.head = null;
+        this.length = 1;
     }
     append(value) {
         let newNode = new Node(value);
         if(!this.head) {
+            this.length++;
             this.head = newNode;
             return this.head;
         }
         let tail = this.head;
         while(tail.next !== null){
             tail = tail.next;
+            this.length++;
         }
         tail.next = newNode;
         return this.head;
     }
-    insertBefore(value, newVal) { // Need to search value via loop and stop once there
-        let newNode = new Node(newVal); // Create new node object
+    insertBefore(value, newVal) { //Sarkis - Need to search value via loop and stop once there
+        let newNode = new Node(newVal); //Sarkis - Create new node object
         if(!this.head) { // If head is not null
             throw new Error('Error');
         }
         if(this.head.value == value) {
+            newNode.next = this.head;
+            this.head = newNode;
             return this.head;
         }
         let current = this.head; // Current is assigned the head object occupying the same spot
-        while(current.next){ // Keep looping until null(final value) reached
-            // Need to check if current value matches input value
+        while(current.next){ //Sarkis - Keep looping until null(final value) reached
+            //Sarkis - Need to check if current value matches input value
             if(current.next.value == value) {
-                // If match found new Node(newVal) --> current(head) object
-                newNode.next = current.next; // new node is assigned current(head)
-                this.head.next = newNode; // Prior node needs to be assigned to newNode
+                this.length++;
+                //Sarkis - If match found new Node(newVal) --> current(head) object
+                newNode.next = current.next; //Sarkis - new node is assigned current(head)
+                return current.next = newNode;  //Sarkis - Prior node needs to be assigned to newNode
             }
             current = current.next;
         }
         return this.head;
     }
-    insertAfter(value, newVal) { // Need to search value via loop and stop once there
+    insertAfter(value, newVal) { //Sarkis - Need to search value via loop and stop once there
         let newNode = new Node(newVal);
         if(!this.head) {
             throw new Error('Error');
         }
         if(this.head.value == value) {
+            this.head.next = newNode;
             return this.head;
         }
         let current = this.head;
         while(current.next){
             if(current.next.value == value) {
                 current = current.next;
-                newNode.next = current.next; // new node is assigned current(head)
+                newNode.next = current.next; //Sarkis - new node is assigned current(head)
                 current.next = newNode;
-                // current = newNode;
+                this.length++;
             }
             current = current.next;
         }
@@ -61,11 +67,15 @@ class LinkedList{
     }
 }
 
-
 let list = new LinkedList();
 list.append(1);
-list.append(2);
-list.append(3);
-list.insertBefore(3, 'a')
+// list.append(2);
+// list.append(3);
+list.insertAfter(1, 'a');
+// list.append(4);
 
-console.log(list)
+
+console.log(list.head.value)
+console.log(list.head)
+
+module.exports = LinkedList;
